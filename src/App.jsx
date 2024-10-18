55fa712fb539830ca6530bb9d8f3b97f0fffacc8
Giovannabe <giovannaaeleodoraaberaldoo@gmail.com>
2024-10-18 11:27:42 -0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
-       
+       <header>
+        <section class="BoxLogo">
+          <img src="/images/logo.png" alt="" />
+        </section>
+        <nav>
+            <ul>
+                <li><a href="">Ofertas</a></li>
+                <li><a href="">Produtos</a></li>
+                <li><a href="">Carrinho</a></li>
+                <li><a href="Entrar">Entrar</a></li>
+            </ul>
+        </nav>
+         </header>
+        <main>
+          <div>
 
+          </div>
+        </main>
+   
+ 
+            
 
 
 
